e3aa72ffab016182d6e4df725801de94d789d642
caesco17 <caescobar17@gmail.com>
2021-04-12 00:41:55 -0600

    Init
    First Commit, API to interact DB and UWP test app.

---
+++ ApiWeb/Controllers/MessagesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiWeb.Controllers
+{
+    public class MessagesController : ApiController
+    {
+        private cbs_testEntities db = new cbs_testEntities();
+
+        public MessagesController()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+
+        public IQueryable<tbl_Messages> Get()
+        {
+            return db.tbl_Messages;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(string Created_Date, string SendTo, string MessageValue)
+        {
+            tbl_Messages msg = new tbl_Messages();
+
+            msg.Created_Date = Convert.ToDateTime(Created_Date);
+            msg.SendTo = SendTo;
+            msg.MessageValue = MessageValue;
+
+            db.tbl_Messages.Add(msg);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = msg.MessageId }, msg);
+        }
+    }
+}
---
+++ ApiWeb/Controllers/SendingMessagesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiWeb.Controllers
+{
+    public class SendingMessagesController : ApiController
+    {
+        private cbs_testEntities db = new cbs_testEntities();
+        [HttpPost]
+        public IHttpActionResult Post(int MessageId, string Sent_Date, string Confirmation_Code)
+        {
+            tbl_SendingMessages msg = new tbl_SendingMessages();
+
+            msg.Sent_Date = Convert.ToDateTime(Sent_Date);
+            msg.MessageId = MessageId;
+            msg.Confirmation_Code = Confirmation_Code;
+
+            try
+            {
+                db.tbl_SendingMessages.Add(msg);
+                db.SaveChanges();
+
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = msg.MessageId }, msg);
+        }
+    }
+}
